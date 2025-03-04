9471e8b64df809cd23965b2600197abc8685ae0e
amanita4t <amanueltekalign4@gmail.com>
2025-03-04 20:48:14 +0300

    updates on views

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
 from django.contrib import messages
+from .models import Profile
+
 def index(request):
     return render(request, 'index.html')
 
@@ -14,9 +16,24 @@ def signup(request):
         password2 = request.POST['password2']
 
         if password==password2:
-            if User.objects.filter(email=email).exists:
-                messages.info(request, 'Email already taken')
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
                 return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user= user_model.id)
+                new_profile.save()
+
+                #debugging
+                messages.success(request, 'Account created successfully! Please log in.')
+                print("Redirecting to signin...")  # ✅ Debugging
+                return redirect('signin')
         else:
             messages.info(request, 'Password not matching!')
             return redirect('signup')
