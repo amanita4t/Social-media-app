59e428df2f1e2208ab243180850e4ecea0771231
amanita4t <amanueltekalign4@gmail.com>
2025-03-18 22:26:34 +0300

    Account setting modified

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -11,6 +11,10 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, 'index.html')
 
+@login_required (login_url='signin')
+def settings(request):
+    return render(request, 'setting.html')
+
 def signup(request):
 
     if request.method == "POST":
@@ -30,14 +34,14 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user= user_model.id)
                 new_profile.save()
 
-                #debugging
-                messages.success(request, 'Account created successfully! Please log in.')
-                print("Redirecting to signin...")  # ✅ Debugging
-                return redirect('signin')
+
+                return redirect('settings')
         else:
             messages.info(request, 'Password not matching!')
             return redirect('signup')
@@ -50,14 +54,13 @@ def signin(request):
         password = request.POST['password']
 
         user = auth.authenticate(username=username, password=password)
-        print(f"User authentication result: {user}")  # Debugging output
+
 
         if user is not None:
             auth.login(request, user)
             return redirect('index')
         else:
             messages.info(request, 'Credentials Invalid')
-            print("Invalid credentials - redirecting to signin")  # Debugging output
             return redirect('signin')
     else:
         return render(request, 'signin.html')
